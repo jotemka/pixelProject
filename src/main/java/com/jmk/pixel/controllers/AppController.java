8e18925adc54fef66fbead872a11c31aec776cc5
Jakub Krysiak <jakub.m.krysiak@gmail.com>
2020-10-20 13:21:02 +0200

    Creating basic classes.

---
+++ src/main/java/com/jmk/pixel/controllers/AppController.java
@@ -0,0 +1,9 @@
+package com.jmk.pixel.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/algorithms")
+public class AppController {
+}
---
+++ src/main/java/com/jmk/pixel/helpers/Helper.java
@@ -0,0 +1,5 @@
+package com.jmk.pixel.helpers;
+
+public class Helper {
+    
+}
