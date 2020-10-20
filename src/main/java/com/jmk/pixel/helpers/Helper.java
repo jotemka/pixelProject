062a6a63a52a76e3752b218c9793c4b09a1a49b1
Jakub Krysiak <jakub.m.krysiak@gmail.com>
2020-10-20 13:54:25 +0200

    Added method for checking if number is correct according to Luhn's algorithm. Added method for splitting numbers into digits.

--- src/main/java/com/jmk/pixel/helpers/Helper.java
+++ src/main/java/com/jmk/pixel/helpers/Helper.java
@@ -7,12 +7,8 @@ public class Helper {
     //obliczanie cyfry kontrolej algorytmem luhna
     public static Integer calculateControlDigit(Integer number){
         int controlNumber = 0;
-        char [] digitsAsChars = String.valueOf(number).toCharArray();
 
-        ArrayList<Integer> digits = new ArrayList<>();
-        for(int i = 0; i<digitsAsChars.length; i++){
-            digits.add(Integer.parseInt(String.valueOf(digitsAsChars[i])));
-        }
+        ArrayList<Integer> digits = splitNumber(number);
 
         int position = digits.size();
         for(int i=0; i<digits.size(); i++){
@@ -38,5 +34,46 @@ public class Helper {
         }
 
         return controlNumber;
+    }
+
+    //sprawdzenie czy ciag liczb jest poprawny
+    public static Boolean checkIfNumberIsCorrect(Integer number){
+        int sum = 0;
+
+        ArrayList<Integer> digits = splitNumber(number);
+
+        int position = digits.size()-1;
+        for(int i=0; i<digits.size(); i++){
+            if(position%2 != 0){
+                int doubled = digits.get(i) * 2;
+                if(doubled >= 10){
+                    int digitSum = 0;
+                    digitSum = (doubled % 10) + (doubled / 10);
+                    doubled = digitSum;
+                }
+                digits.set(i, doubled);
+            }
+            position = position - 1;
+        }
+
+        for(Integer i : digits){
+            sum += i;
+        }
+        sum = sum%10;
+        if(sum==0){
+            return true;
+        }
+        return false;
+    }
+
+    private static ArrayList<Integer> splitNumber(Integer number){
+        char [] digitsAsChars = String.valueOf(number).toCharArray();
+
+        ArrayList<Integer> split = new ArrayList<>();
+        for(int i = 0; i<digitsAsChars.length; i++){
+            split.add(Integer.parseInt(String.valueOf(digitsAsChars[i])));
+        }
+
+        return split;
     }
 }
