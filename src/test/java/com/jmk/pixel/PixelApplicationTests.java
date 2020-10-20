d9d75d37b60b80c996e1e3a8b0acfda1b62bc7d3
Jakub Krysiak <jakub.m.krysiak@gmail.com>
2020-10-20 13:16:22 +0200

    Project setup.

---
+++ src/test/java/com/jmk/pixel/PixelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmk.pixel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
